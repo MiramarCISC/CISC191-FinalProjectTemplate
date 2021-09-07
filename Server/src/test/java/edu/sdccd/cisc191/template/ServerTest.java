3d2cdd27050cbd5cc9c7332d261647055993a42e
Jonathan Plata <jonathan_plata@ymail.com>
2021-09-07 06:48:49 -0700

    Finished menu operations and added add,edit functions to manipulate the sales data. TODO: unit tests

---
+++ Server/src/test/java/edu/sdccd/cisc191/template/ServerTest.java
@@ -0,0 +1,24 @@
+package edu.sdccd.cisc191.template;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServerTest {
+
+    @Test
+    void outputData() {
+    }
+
+    @Test
+    void addData() {
+    }
+
+    @Test
+    void editData() {
+    }
+
+    @Test
+    void createDataSet() {
+    }
+}
