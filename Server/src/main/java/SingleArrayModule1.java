36212356f51395e81ff10633c8ad0438df018108
iangge <iangeusebio@gmail.com>
2022-08-27 16:33:21 -0700

    All options work, but it is possible to get a null value while taking attendance

--- Server/src/main/java/SingleArrayModule1.java
+++ Server/src/main/java/SingleArrayModule1.java
@@ -9,17 +9,21 @@ public class SingleArrayModule1 {
     }
 
     public static void main(String args[]) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of students: ");
+        int numOfStudents = scanner.nextInt();
+
+
         // Boolean array for studentAttendance
         // true means student is here, false means student is absent
-        Boolean[] studentAttendance = new Boolean[10];
-
+        Boolean[] studentAttendance = new Boolean[numOfStudents];
 
         String[] options = {"1- take attendance",
-                "2- mark student absent",
+                "2- edit student attendance",
                 "3- view student attendance",
                 "4- Exit"
         };
-        Scanner scanner = new Scanner(System.in);
         int option = 1;
         while (option != 4) {
             printMenu(options);
@@ -27,13 +31,13 @@ public class SingleArrayModule1 {
                 option = scanner.nextInt();
                 switch (option) {
                     case 1:
-                        option1();
+                        option1(studentAttendance);
                         break;
                     case 2:
-                        option2();
+                        option2(studentAttendance);
                         break;
                     case 3:
-                        option3();
+                        option3(studentAttendance);
                         break;
                     case 4:
                         System.exit(0);
@@ -48,14 +52,49 @@ public class SingleArrayModule1 {
     }
 
     // Options
-    private static void option1() {
-        System.out.println("Thanks for choosing option 1");
+    private static void option1(Boolean[] studentAttendance) {
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < studentAttendance.length; i++) {
+            System.out.println("Is student[" + i + "] here? (Y/N)");
+                char yesOrNo;
+                yesOrNo = scanner.next().charAt(0);
+                Character.toLowerCase(yesOrNo);
+                if (yesOrNo == 'y') {
+                    studentAttendance[i] = true;
+                } else if (yesOrNo == 'n') {
+                    studentAttendance[i] = false;
+                } else {
+                    System.out.println("Invalid value for Student[" + i + "]");
+                }
+                }
+            }
+
+
+
+    private static void option2(Boolean[] studentAttendance) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input student's index number: ");
+        int studentIndexNumber;
+        studentIndexNumber = scanner.nextInt();
+        System.out.println("Is this student here? (Y/N)");
+        char yesOrNo;
+        yesOrNo = scanner.next().charAt(0);
+        Character.toLowerCase(yesOrNo);
+        if (yesOrNo == 'y') {
+            studentAttendance[studentIndexNumber] = true;
+            System.out.println("Student[" + studentIndexNumber + "] " +
+                    "marked as present.");
+        } else if (yesOrNo == 'n') {
+            studentAttendance[studentIndexNumber] = false;
+            System.out.println("Student[" + studentIndexNumber + "] " +
+                    "marked as absent.");
+        }
     }
-    private static void option2() {
-        System.out.println("Thanks for choosing option 2");
-    }
-    private static void option3() {
-        System.out.println("Thanks for choosing option 3");
+    private static void option3(Boolean[] studentAttendance) {
+        for (int i = 0; i < studentAttendance.length; i++) {
+            System.out.println("Student[" + i + "] attendance = " +
+                    studentAttendance[i]);
+        }
     }
 }
 
