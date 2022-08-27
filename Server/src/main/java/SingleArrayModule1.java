3251eb1b3f320294a63c7cd6763cdcf7d4918883
iangge <iangeusebio@gmail.com>
2022-08-27 14:58:55 -0700

    Set up the placeholder options

--- Server/src/main/java/SingleArrayModule1.java
+++ Server/src/main/java/SingleArrayModule1.java
@@ -20,11 +20,42 @@ public class SingleArrayModule1 {
                 "4- Exit"
         };
         Scanner scanner = new Scanner(System.in);
-        int option;
-        while (true) {
+        int option = 1;
+        while (option != 4) {
             printMenu(options);
-            option = scanner.nextInt();
+            try {
+                option = scanner.nextInt();
+                switch (option) {
+                    case 1:
+                        option1();
+                        break;
+                    case 2:
+                        option2();
+                        break;
+                    case 3:
+                        option3();
+                        break;
+                    case 4:
+                        System.exit(0);
+                }
+            } catch (Exception ex){
+                System.out.println("Please enter an integer value between 1 and " +
+                                options.length);
+                    scanner.next();
+            }
         }
 
     }
+
+    // Options
+    private static void option1() {
+        System.out.println("Thanks for choosing option 1");
+    }
+    private static void option2() {
+        System.out.println("Thanks for choosing option 2");
+    }
+    private static void option3() {
+        System.out.println("Thanks for choosing option 3");
+    }
 }
+
