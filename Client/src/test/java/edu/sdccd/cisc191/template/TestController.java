9cf4102b47efd513ecb899c348b10fd07ba5db49
iangge <iangeusebio@gmail.com>
2022-09-30 10:04:40 -0700

    Last working commit

--- Client/src/test/java/edu/sdccd/cisc191/template/TestController.java
+++ Client/src/test/java/edu/sdccd/cisc191/template/TestController.java
@@ -1,13 +1,37 @@
 package edu.sdccd.cisc191.template;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+
+import javafx.embed.swing.JFXPanel;
 import javafx.scene.control.Button;
-import org.junit.jupiter.api.TestInstance;
+import javafx.scene.control.TextField;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestController {
+    @Test
+    void testSet() {
+        JFXPanel panel = new JFXPanel();
+        // tricks JUNIT
+        Button button = new Button();
+        TextField textfield = new TextField();
 
+        Controller controller = new Controller();
+
+        controller.setTextFieldAndButton(textfield, "test", button, "test");
+
+        assertEquals("test", textfield.getPromptText());
+    }
+
+    // Make interfaces classes with testable methods
+    // instatiate question, get answer, assertEqual question and answer
+    // for loop
+    // Hash Map questions to answers
+    // Build upon in September
+
+
+    // method that creates button and sets controller method
+    // test button creation
+    // Have method return button
     @Test
     void testCheckAnswer() {
         Controller controller = new Controller();
@@ -21,7 +45,7 @@ public class TestController {
 
         controller.checkAnswer(wrongAnswer, answer);
 
-
+        // Test Per topic you're demonstrating
 
 
     }
