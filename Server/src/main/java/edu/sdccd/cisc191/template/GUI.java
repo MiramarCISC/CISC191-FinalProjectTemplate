0dc349f65219005f4b1b709ee5185649981489e7
Devan <devan.odin.anderson@gmail.com>
2022-10-24 13:04:34 -0700

    Created a Board class, GUI class, and player class. Using gui form to create a gui

---
+++ Server/src/main/java/edu/sdccd/cisc191/template/GUI.java
@@ -0,0 +1,11 @@
+package edu.sdccd.cisc191.template;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+//public class GUI {
+
+//}
