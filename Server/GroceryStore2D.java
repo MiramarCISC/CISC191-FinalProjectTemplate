0ed0fa7c1d84c6007b01eb35b09cd1b3967f366b
ascarloswillis <144636730+ascarloswillis@users.noreply.github.com>
2023-11-11 19:19:48 -0800

    Update GroceryStore2D.java

--- Server/GroceryStore2D.java
+++ Server/GroceryStore2D.java
@@ -1,37 +1,47 @@
 package org.example;
-import java.util.Arrays;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
 
 public class GroceryStore2D {
-    private GroceryItem[][] items;
+    private ArrayList<LinkedList<GroceryItem>> rows;
     private int numRows;
     private int numCols;
 
     public GroceryStore2D(int initialRows, int initialCols) {
-        this.items = new GroceryItem[initialRows][initialCols];
+        this.rows = new ArrayList<>(initialRows);
+        for (int i = 0; i < initialRows; i++) {
+            rows.add(new LinkedList<>());
+        }
         this.numRows = initialRows;
         this.numCols = initialCols;
     }
 
     public GroceryItem getAtIndex(int row, int col) {
         if (isValidIndex(row, col)) {
-            return items[row][col];
+            LinkedList<GroceryItem> items = rows.get(row);
+            if (col >= 0 && col < items.size()) {
+                return items.get(col);
+            }
         }
         return null;
     }
 
     public void setAtIndex(int row, int col, GroceryItem item) {
         if (isValidIndex(row, col)) {
-            items[row][col] = item;
+            LinkedList<GroceryItem> items = rows.get(row);
+            if (col >= 0 && col < items.size()) {
+                items.set(col, item);
+            }
         }
     }
 
     public int findIndexOf(GroceryItem item) {
         for (int i = 0; i < numRows; i++) {
-            for (int j = 0; j < numCols; j++) {
-                if (items[i][j] != null && items[i][j].equals(item)) {
-                    return i * numCols + j;
-                }
+            LinkedList<GroceryItem> items = rows.get(i);
+            int colIndex = items.indexOf(item);
+            if (colIndex != -1) {
+                return i * numCols + colIndex;
             }
         }
         return -1;
@@ -39,9 +49,10 @@ public class GroceryStore2D {
 
     public void printAll() {
         for (int i = 0; i < numRows; i++) {
+            LinkedList<GroceryItem> items = rows.get(i);
             for (int j = 0; j < numCols; j++) {
-                if (items[i][j] != null) {
-                    System.out.println("[" + i + "][" + j + "] " + items[i][j]);
+                if (j < items.size()) {
+                    System.out.println("[" + i + "][" + j + "] " + items.get(j));
                 }
             }
         }
@@ -49,27 +60,26 @@ public class GroceryStore2D {
 
     public void deleteAtIndex(int row, int col) {
         if (isValidIndex(row, col)) {
-            items[row][col] = null;
+            LinkedList<GroceryItem> items = rows.get(row);
+            if (col >= 0 && col < items.size()) {
+                items.remove(col);
+            }
         }
     }
 
     public void expand(int newRows, int newCols) {
         if (newRows > numRows && newCols > numCols) {
-            items = Arrays.copyOf(items, newRows);
-            for (int i = numRows; i < newRows; i++) {
-                items[i] = new GroceryItem[newCols];
+            while (numRows < newRows) {
+                rows.add(new LinkedList<>());
+                numRows++;
             }
-            numRows = newRows;
             numCols = newCols;
         }
     }
 
     public void shrink(int newRows, int newCols) {
         if (newRows >= 0 && newRows < numRows && newCols >= 0 && newCols < numCols) {
-            items = Arrays.copyOf(items, newRows);
-            for (int i = 0; i < newRows; i++) {
-                items[i] = Arrays.copyOf(items[i], newCols);
-            }
+            rows.subList(newRows, numRows).clear();
             numRows = newRows;
             numCols = newCols;
         }
@@ -78,6 +88,4 @@ public class GroceryStore2D {
     private boolean isValidIndex(int row, int col) {
         return row >= 0 && row < numRows && col >= 0 && col < numCols;
     }
-
-
 }
