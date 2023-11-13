8d40b0c508ffd50e1534af2f2d48433bba855e70
ascarloswillis <144636730+ascarloswillis@users.noreply.github.com>
2023-11-12 16:25:33 -0800

    Update CustomerResponse.java

--- Common/src/main/java/edu/sdccd/cisc191/template/CustomerResponse.java
+++ Common/src/main/java/edu/sdccd/cisc191/template/CustomerResponse.java
@@ -1,57 +1,16 @@
-package edu.sdccd.cisc191.template;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-public class CustomerResponse {
-    private Integer id;
-    private String firstName;
-    private String lastName;
-
-    @JsonIgnore
-    private static final ObjectMapper objectMapper = new ObjectMapper();
-    public static String toJSON(CustomerResponse customer) throws Exception {
-        return objectMapper.writeValueAsString(customer);
+package edu.sdccd.cisc191;
+import java.io.Serializable;
+public class GroceryRequest implements Serializable {
+    private String category;
+    private String name;
+    public GroceryRequest(String category, String name) {
+        this.category = category;
+        this.name = name;
     }
-    public static CustomerResponse fromJSON(String input) throws Exception{
-        return objectMapper.readValue(input, CustomerResponse.class);
+    public String getCategory() {
+        return category;
     }
-    protected CustomerResponse() {}
-
-    public CustomerResponse(Integer id, String firstName, String lastName) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-
-    @Override
-    public String toString() {
-        return String.format(
-                "Customer[id=%d, firstName='%s', lastName='%s']",
-                id, firstName, lastName);
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+    public String getName() {
+        return name;
     }
 }
