ae181b33716474bbdc4d0d6863589abbf7be38ad
Oliver Tran <olivertran114@gmail.com>
2024-02-23 17:53:41 -0800

    Added comments to improve readability To Do: Preserve spaces and capitalization

--- Client/src/main/java/edu/sdccd/cisc191/ciphers/Hill.java
+++ Client/src/main/java/edu/sdccd/cisc191/ciphers/Hill.java
@@ -5,18 +5,25 @@ import java.io.IOException;
 public class Hill {
     private static final int[] MULT_INVERSE= {1,0,9,0,21,0,15,0,3,0,19,0,0,0,7,0,23,0,11,0,5,0,17,0,25};
 
-    public static String encode(String plainText, String key) {
-        plainText = plainText.toUpperCase().replaceAll("[^A-Z]", "");
+    /**************************************************************************
+     * Encrypts plain text using a Hill Cipher given a key word
+     *************************************************************************/
+    public static String encode(String inputText, String key) {
+        //Removes all non alphabet characters and spaces
+        String plainText = inputText.toUpperCase().replaceAll("[^A-Z]", "");
         StringBuilder cipherText = new StringBuilder();
         int[][] keyMatrix = createKeyMatrix(key);
         int matrixLength = keyMatrix.length;
 
+        //Append Z's to allow key matrix to transform entire plain text
         if(plainText.length()%matrixLength != 0)
             for(int i=0; i<plainText.length()%matrixLength; i++) {
                 plainText = plainText.concat("Z");
             }
 
+        //Transforms plain text matrix.length letters at a time
         for(int i=0; i<=plainText.length()-matrixLength; i+=matrixLength) {
+            //Arranges letters as a vector
             int[] letterVector = new int[matrixLength];
             for(int c=0; c<letterVector.length; c++){
                 letterVector[c%matrixLength] = plainText.charAt(c+i) - 'A';
@@ -24,6 +31,7 @@ public class Hill {
 
             letterVector = matrixMultiply(keyMatrix, letterVector);
 
+            //Appends output with transformed vector
             for(int c : letterVector) {
                 cipherText.append((char)(c + 'A'));
             }
@@ -31,13 +39,22 @@ public class Hill {
 
         return cipherText.toString();
     }
+
+    /**************************************************************************
+     * Decodes cipher text using a Hill Cipher given the encryption key
+     *************************************************************************/
     public static String decode(String cipherText, String key){
+        //Removes all spaces and non-alphabetic characters
         cipherText = cipherText.toUpperCase().replaceAll("[^A-Z]", "");
         StringBuilder plainText = new StringBuilder();
+
+        //Creates decryption matrix by inverting the key matrix
         int[][] decryptionMatrix = findInverse(createKeyMatrix(key));
         int matrixLength = decryptionMatrix.length;
 
+        //Transforms plain text matrix.length letters at a time
         for(int i=0; i<=cipherText.length()-matrixLength; i+=matrixLength) {
+            //Arranges letters into a vector
             int[] letterVector = new int[matrixLength];
             for(int c=0; c<letterVector.length; c++){
                 letterVector[c%matrixLength] = cipherText.charAt(c+i) - 'A';
@@ -45,6 +62,7 @@ public class Hill {
 
             letterVector = matrixMultiply(decryptionMatrix, letterVector);
 
+            //Appends output with transformed vector
             for(int c : letterVector) {
                 plainText.append((char)(c + 'A'));
             }
@@ -53,7 +71,12 @@ public class Hill {
         return plainText.toString();
     }
 
+    /**************************************************************************
+     * Creates square matrix, looping through the word if the word length
+     * isn't a perfect square
+     *************************************************************************/
     private static int[][] createKeyMatrix(String key) {
+        //Find dimensions required to contain entire key
         int n = (int) (Math.sqrt(key.length())+0.99);
         int[][] keyMatrix = new int[n][n];
 
@@ -66,6 +89,9 @@ public class Hill {
         return keyMatrix;
     }
 
+    /**************************************************************************
+     * Multiplies matrix by vector
+     *************************************************************************/
     private static int[] matrixMultiply(int[][] matrix, int[] vector) {
         int n = matrix.length;
         int[] output = new int[n];
@@ -81,21 +107,29 @@ public class Hill {
         return output;
     }
 
+    /**************************************************************************
+     * Finds determinant using Laplace Expansion by calling itself recursively
+     *************************************************************************/
     private static int findDeterminant(int[][] matrix){
         int det = 0, sign = 1;
 
-        if(matrix.length > 2) {
-            for (int i = 0; i < matrix[0].length; i++) {
-                det += matrix[0][i] * findDeterminant(getSubMatrix(matrix, 0, i)) * sign;
-                sign *= -1;
-            }
-        } else{
+        //Find 2x2 determinant
+        if(matrix.length == 2){
             return matrix[0][0]*matrix[1][1] - matrix[0][1]*matrix[1][0];
+        }
+
+        //Find nxn determinant where n>2
+        for (int i = 0; i < matrix[0].length; i++) {
+            det += matrix[0][i] * findDeterminant(getSubMatrix(matrix, 0, i)) * sign;
+            sign *= -1;
         }
 
         return det;
     }
 
+    /**************************************************************************
+     * Returns minor matrix for laplace expansion
+     *************************************************************************/
     public static int[][] getSubMatrix(int[][] matrix, int i, int j) {
         int[][] subMatrix = new int[matrix.length - 1][matrix[0].length - 1];
         for(int row=0,outRow=0; row<matrix.length; row++) {
@@ -112,8 +146,13 @@ public class Hill {
         return subMatrix;
     }
 
+    /**************************************************************************
+     * Finds inverse matrix using Gauss-Jordan elimination
+     *************************************************************************/
     public static int[][] findInverse(int[][] inputMatrix) {
         int det = findDeterminant(inputMatrix);
+
+        //Throws exception if matrix is non-invertible mod26
         try {
             if (det % 2 == 0 || det == 13)
                 throw new IOException("The key is non-invertible");
@@ -125,22 +164,28 @@ public class Hill {
         int[][] inverse = new int[inputMatrix.length][inputMatrix[0].length];
         int inverseDet = MULT_INVERSE[((det%26)+26)%26 - 1];
 
+        //Gauss-Jordan Elimination
         for(int i=0; i<matrix.length; i++) {
-            matrix[i] = scaleRow(matrix[i], 1/matrix[i][i]);
-            for(int j=0; j<matrix.length; j++) {
+            matrix[i] = scaleRow(matrix[i], 1/matrix[i][i]);    //Scales pivot to equal 1
+            for(int j=0; j<matrix.length; j++) {    //Performs row reduction
                 if(j!=i)
                     matrix[j] = addRow(matrix[j], scaleRow(matrix[i], -1*matrix[j][i]));
             }
         }
 
+        //Set output matrix by iterating through augmented matrix
         for(int i=0; i<matrix.length; i++) {
             for(int j=matrix.length; j<matrix[0].length; j++) {
+                //Turns output into its mod26 inverse
                 inverse[i][j-matrix.length] = (int) (Math.round(((matrix[i][j] * det * inverseDet)%26))+26)%26;
             }
         }
         return inverse;
     }
 
+    /**************************************************************************
+     * Sets 1st row equal to the sum of both rows
+     *************************************************************************/
     public static double[] addRow(double[] outputVector, double[] b) {
         for(int i=0; i<outputVector.length; i++) {
             outputVector[i] += b[i];
@@ -148,6 +193,9 @@ public class Hill {
         return outputVector;
     }
 
+    /**************************************************************************
+     * Scales row by given scalar
+     *************************************************************************/
     public static double[] scaleRow(double[] row, double scalar) {
         double[] output = new double[row.length];
         for(int i=0; i<row.length; i++) {
@@ -156,6 +204,9 @@ public class Hill {
         return output;
     }
 
+    /**************************************************************************
+     * Augments identity matrix to the key matrix
+     *************************************************************************/
     public static double[][] augmentIdentityMatrix(int[][] matrix) {
         double[][] ret = new double[matrix.length][matrix[0].length*2];
         for(int i=0; i<matrix.length; i++){
